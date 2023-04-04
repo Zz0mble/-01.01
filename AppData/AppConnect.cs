7bc2e6391d1ad52a20792aa7d44e9d31afbe2905
Титов Вадим <106253286+Zz0mble@users.noreply.github.com>
2023-04-04 23:32:42 +0700

    Add files via upload

---
+++ AppData/AppConnect.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESUN.AppData
+{
+    class AppConnect
+    {
+        public static ESUNEntities2 model0db;
+    }
+}
